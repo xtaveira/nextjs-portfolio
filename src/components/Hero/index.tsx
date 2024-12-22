df264234613adc4e156975cc4432161a6f7b42c8
Davi Taveira <devtaveira@gmail.com>
2024-12-22 08:05:36 -0300

    some updates on hero and readme

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <div className="w-full flex flex-col gap-3 items-center py-12 bg-red-500">
+      <p>Project In Progress, Still Working on Header...</p>
+      <p>Follow the taks on GitHub: </p>
+    </div>
+  );
+};
+
+export default Hero;
