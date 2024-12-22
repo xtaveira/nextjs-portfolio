e76ad069ed1261b2434b25b18d94cc8cfb7777df
Davi Taveira <devtaveira@gmail.com>
2024-12-22 08:09:47 -0300

    updates on hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="flex-1 flex flex-col ">
       <Header />
       <Separator />
+      <Hero />
 
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,8 +1,17 @@
 const Hero = () => {
   return (
-    <div className="w-full flex flex-col gap-3 items-center py-12 bg-red-500">
+    <div className="w-full flex flex-col gap-3 items-center py-12 ">
       <p>Project In Progress, Still Working on Header...</p>
-      <p>Follow the taks on GitHub: </p>
+      <p>
+        Follow the taks on GitHub:{" "}
+        <a
+          className="text-orange-500 font-bold"
+          target="_blank"
+          href="https://github.com/users/xtaveira/projects/1/views/1"
+        >
+          Tasks
+        </a>
+      </p>
     </div>
   );
 };
