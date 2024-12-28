12d45dcfb9b33722bb63f98b39e1de10d12beb0d
Davi Taveira <devtaveira@gmail.com>
2024-12-28 16:26:32 -0300

    some fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ export default function Home() {
     <div className="flex-1 flex flex-col ">
       <Header />
       <Separator />
-
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
